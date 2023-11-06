4fa896b3aad3cf80209eaf4e671aa21fa61186bc
ADAM AMAZIANE <adamamaziane2005@gmail.com>
2023-11-06 18:31:51 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ImageSlider from './component/ImageSlider';
+import { SliderData } from './component/SliderData';
+
+function App() {
+  return (
+    <div className="App">
+     <ImageSlider slides={SliderData} />
+
+    </div>
+  );
+}
+// ana an 7awwikkkk skot khalini khadam
+export default App;
---
+++ src/component/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+    {
+        image:
+        'https://static.anime21.blog.br/2022/10/01-11.jpg'
+      },
+      {
+        image:
+          'https://otakusinh.files.wordpress.com/2022/10/nishino-akane.png?w=1024'
+      },
+      {
+        image:
+          'https://staticg.sportskeeda.com/editor/2022/10/fec4a-16650798464186-1920.jpg'
+      },
+      {
+        image:
+          '  https://i0.wp.com/www.crowsworldofanime.com/wp-content/uploads/2022/10/The_Eminence_in_Shadow-Episode_01-Figure_05.jpg?fit=2278%2C1280&ssl=1 '
+      },
+      {
+        image:
+          'https://staticg.sportskeeda.com/editor/2023/02/bceba-16765358999741-1920.jpg?w=840'
+      }
+];
