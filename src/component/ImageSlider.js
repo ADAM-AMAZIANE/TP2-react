577c73517c5c9a864cc32a914d7e3934eed26390
ADAM AMAZIANE <adamamaziane2005@gmail.com>
2023-11-18 15:57:55 +0100

    first edit

--- src/component/ImageSlider.js
+++ src/component/ImageSlider.js
@@ -1,29 +1,30 @@
+// ImageSlider.jsx
+
 import React, { useState } from 'react';
-import { SliderData } from './SliderData';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 import './Slider.css';
 
 const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
-  const length = slides.length;
+  const cont = slides.length;
 
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+    setCurrent(current === cont - 1 ? 0 : current + 1);
   };
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
+    setCurrent(current === 0 ? cont - 1 : current - 1);
   };
 
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
- 
+
   return (
     <section className='slider-container'>
       <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
       <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-      {SliderData.map((slide, index) => {
+      {slides.map((slide, index) => {
         return (
           <div
             className={index === current ? 'slide active' : 'slide'}
